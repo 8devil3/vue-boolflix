51ce85f777b042efcb329a8066df8fc695d5ec10
8devil3 <8dev.il3@gmail.com>
2022-03-29 12:56:08 +0200

    aggiunte locandine

--- src/components/MainSite.vue
+++ src/components/MainSite.vue
@@ -9,6 +9,7 @@
             <p v-if="mDetails.original_language == '' || mDetails.original_language == 'xx'">Nessuna lingua</p>
             <lang-flag v-else :iso="mDetails.original_language" :squared="false" />
             <p>{{ mDetails.vote_average }}</p>
+            <img :src="imgBaseURL + imgSizeSmall + mDetails.poster_path" :alt="mDetails.title">
         </li>
     </ul>
     <h2>serie TV</h2>
@@ -20,6 +21,7 @@
             <p v-if="sDetails.original_language == '' || sDetails.original_language == 'xx'">Nessuna lingua</p>
             <lang-flag v-else :iso="sDetails.original_language" :squared="false" />
             <p>{{ sDetails.vote_average }}</p>
+            <img :src="imgBaseURL + imgSizeSmall + sDetails.poster_path" :alt="sDetails.title">
         </li>
     </ul>
 </main>
@@ -31,7 +33,17 @@ import LangFlag from 'vue-lang-code-flags'
 export default {
     name: 'MainSite.vue',
     components: {
-        LangFlag
+        LangFlag //https://github.com/qWici/vue-lang-code-flags
+    },
+    data(){
+        return {
+            imgBaseURL: 'https://image.tmdb.org/t/p/',
+            imgSizeSmall: 'w154',
+            imgSizeMedium: 'w342',
+            imgSizeLarge: 'w500',
+            imgSizeBig: 'w780',
+            imgURL: '',
+        }
     },
     props: {
         movieData: Array,
